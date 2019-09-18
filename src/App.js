9146113164a133883d6f467a0af322fe50705049
Dustin Davis <dustin.davis2244@gmail.com>
2019-09-18 20:10:26 +0000

    hooked up the components and changed the states

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Nav from './components/Nav'
-
+import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note'
+                                          //App is child to React.Component
  class App extends Component{
+  constructor(){
+                                          //calling super method allows us to use this keyword, initializes it
+    super();
+                                          //state is the data that changes. whenever the state changes, 
+                                          //React will call the render method on the comp. and apply changes
+    this.state = {
+                                          //React will render Note comp if true, List comp. if false
+      showNote: false
+    };
+  }
+                                //In order to change the value of a comp. state, setState method will be used
+                                  //since showNote is boolean, ! will reverse the T = F, F = T
+                                  //toggleNote is used to change value of showNote
+  toggleNote = () => {
+    this.setState({
+      showNote: ! this.state.showNote
+    });
+  }
+
    render(){
+                                        //using object Destructuring, we made the indiv key accessable 
+                                        //this.state.showNote = the method used and using showNote as the vari.
+    const { showNote } = this.state;
+
     return (
       <div className="App">
-       <Nav />
+       <Nav toggleNote={this.toggleNote} showNote={showNote} />
+                                        {/* state = either (list out the notes) or (interact with 1 single note) */}
+                                        {/* if showNote is T, run Note comp.,, if F, run List comp. */}
+      { showNote ? <Note /> : <List /> } 
       </div>
     );
   }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {Component } from 'react';
 
-class Nav extends React.Component {
+class Nav extends Component {
   render(){
+                                    //this.props will take the property from toggleNote in the Nav component, and making that valuye accessible 
+    const { toggleNote, showNote } = this.props;
+
     return (
-      <div>
-       Nav Component
+      <div className='nav-container'>
+       <div className='nav-logo'>Note</div>
+                                                                    {/* this onClick method will take the toggleNote and pass it thru App.js and that change the state */}
+       <div className='nav-button' onClick={() => toggleNote()} >
+        {showNote ? 'Cancel' : '+ Note' }
+       </div>
       </div>  
     );
   }
